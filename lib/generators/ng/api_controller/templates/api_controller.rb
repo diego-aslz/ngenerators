7495804de366212d96389956e5ab00b5ccf53f26
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2014-09-23 15:13:36 -0300

    Generating scopes on API controller.

--- lib/generators/ng/api_controller/templates/api_controller.rb
+++ lib/generators/ng/api_controller/templates/api_controller.rb
@@ -2,6 +2,16 @@ module Api
   class <%= plural_name.camelize %>Controller < BaseController
     <%- if options.model -%>
     defaults resource_class: <%= options.model.try(:camelize) %>
+
+    <%- end -%>
+    <%- anothers = [] -%>
+    <%- klass.columns.each do |column| -%>
+    <%- if column.type == :date || column.type == :datetime -%>
+    has_scope :by_<%= column_name(column) %>, type: :hash, using: [:since, :until]
+    <%- else anothers << column end -%>
+    <%- end -%>
+    <%- if anothers.any? -%>
+    has_scope <%= anothers.map{ |col| ":by_#{column_name(col)}"}.join(', ') %>
     <%- end -%>
   end
 end
