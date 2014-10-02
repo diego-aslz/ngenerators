3de64782043f811fea680479d3cee9096fe04338
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2014-10-01 23:11:52 -0300

    Generating spec file for form.

--- lib/generators/ng/form/form_generator.rb
+++ lib/generators/ng/form/form_generator.rb
@@ -10,6 +10,10 @@ module Ng
         template 'form_ctrl.coffee', "app/assets/javascripts/ng/controllers/#{singular_name}_form_ctrl.coffee"
       end
 
+      def spec
+        template 'spec.rb.erb', "spec/features/#{singular_name}_show_spec.rb"
+      end
+
       def route
         inject_ng_route do
           "    .state         '#{ng_singular_name}Form',\n"\
